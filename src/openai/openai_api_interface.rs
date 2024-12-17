a5bcf537e7df5cb7af27caf999856af8ecc9c5a7
Lucas Meneses <lucasmenesesbs@gmail.com>
2024-12-17 00:22:50 -0300

    implemented emebeddings interface

---
+++ src/openai/openai_api_interface.rs
@@ -0,0 +1,71 @@
+use crate::openai::openai_endpoints::OpenAiEndpoints;
+use thiserror::Error;
+use reqwest;
+use serde_json;
+use serde;
+use log::info;
+
+#[derive(Error, Debug)]
+pub enum ApiError {
+    #[error("Request error: {0}")]
+    Reqwest(#[from] reqwest::Error),
+
+    #[error("JSON parsing error: {0}")]
+    SerdeJson(#[from] serde_json::Error),
+}
+
+
+#[derive(serde::Deserialize)]
+struct EmbeddingsResponse {
+    data: Vec<Data>,
+}
+#[derive(serde::Deserialize)]
+struct Data {
+    embedding: Vec<f32>,
+}
+
+pub struct OpenAi {
+    api_key: String,
+}
+
+impl OpenAi {
+
+    pub fn new(api_key: String) -> Self{
+        Self { api_key }
+    }
+
+    pub async fn generate_embeddings(&self, text: &str, model: &str) -> Result<Vec<f32>, ApiError> {
+
+        let client = reqwest::Client::new();
+        let embedding_endpoint = OpenAiEndpoints::Embeddings.endpoint();
+
+        let request_body: serde_json::Value = serde_json::json!({
+            "input": text,
+            "model": model,
+            "encoding_format": "float"
+        });
+
+        let response = client
+            .post(embedding_endpoint)
+            .header("Content-Type", "application/json")
+            .header("Authorization", format!("Bearer {}", self.api_key))
+            .body(request_body.to_string())
+            .send()
+            .await?;
+
+        let json_res: serde_json::Value = match response.json().await{
+            Ok(value) => value,
+            Err(e) => {
+                info!("Embeddings generation failed: {}", e);
+                return Err(ApiError::from(e));
+
+            }
+        };
+
+        let embedding_response: EmbeddingsResponse = serde_json::from_value(json_res.clone())?;
+
+        let embeddings = &embedding_response.data[0].embedding;
+
+        Ok(embeddings.to_vec())
+    }
+}
